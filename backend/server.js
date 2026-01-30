e6269969edd7c2cfb0d7122a2fbd3ca03dc70751
gauravs2430 <202351039@iiitvadodara.ac.in>
2026-01-30 16:07:58 +0530

    feat: created userSchema in user.models.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const app = require("./src/app");
 const connectDB = require("./src/db/db")
+
+
+connectDB();
+
 app.listen(3002 , ()=>{
     console.log("Server is listning on 3002");
 
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+router.post("/user/login");
+router.post("/user/register");
+
+
+
+module.exports = router ;
